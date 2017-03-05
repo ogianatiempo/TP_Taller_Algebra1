486e868be5f4d87d84430decc1d2812e9628c9c7
Octavio Gianatiempo <ogianatiempo@gmail.com>
2017-03-05 19:59:47 -0300

    Cambio los lugares donde dice string por el tipo texto que para algo lo definimos

--- Parte2.hs
+++ Parte2.hs
@@ -37,9 +37,9 @@ cifrarCesar (CifradoPalabrasReverso m) n = CifradoPalabrasReverso (cifrarCesar m
 
 -- EJERCICIO 9 ----------------------------------------------------------------
 
--- Dada lista de caracteres devuelve True en las posiciones que hay espacios.
+-- Dada un Texto devuelve True en las posiciones que hay espacios.
 -- Es funcion auxiliar de posicionesEspacios
-esEspacio :: String -> [Bool]
+esEspacio :: Texto -> [Bool]
 esEspacio [] = []
 esEspacio (' ':cs) = True : esEspacio cs
 esEspacio (_:cs) = False : esEspacio cs
@@ -59,39 +59,39 @@ subsetVerdadero [] [] = []
 subsetVerdadero (True:bs) (e:es) = e : subsetVerdadero bs es
 subsetVerdadero (False:bs) (e:es) = subsetVerdadero bs es
 
--- Dada una lista de caracteres devuelve las posiciones de los espacios.
+-- Dada un Texto devuelve las posiciones de los espacios.
 -- El primer caracter es el caracter 0.
 -- Es funcion auxiliar de reversoPalabras
-posicionesEspacios :: String -> [Integer]
+posicionesEspacios :: Texto -> [Integer]
 posicionesEspacios s = subsetVerdadero (esEspacio s) (posiciones s 0)
 
--- Dado un string y un integer, recorta la parte izquierda del string para que
+-- Dado un Texto y un Integer, recorta la parte izquierda del texto para que
 -- comience justo después de la posicion indicada por el integer. Es decir se
 -- queda con la parte derecha. (la primera posicion es 0).
 -- Es funcion auxiliar de listaPalabras
-derecha :: String -> Integer -> String
+derecha :: Texto -> Integer -> Texto
 derecha (c:cs) 0 = cs
 derecha (c:cs) n = derecha cs (n-1)
 
--- Dado un string y un integer, recorta la parte derecha del string justo antes
+-- Dado un texto y un integer, recorta la parte derecha del texto justo antes
 -- de la posicion indicada por el integer. Es decir, se queda con la parte
 -- izquierda. (la primera posicion es 0).
 -- Es funcion auxiliar de listaPalabras
-izquierda :: String -> Integer -> String
+izquierda :: Texto -> Integer -> Texto
 izquierda s 0 = []
 izquierda (c:cs) n = c : izquierda cs (n-1)
 
--- Dado un string, una lista de integers representando los espacios y un Integer
+-- Dado un Texto, una lista de integers representando los espacios y un Integer
 -- que representa el espacio anterior (debe ser 0 al inicio), separa
--- las palabras del string y las pone en una lista.
+-- las palabras del Texto y las pone en una lista.
 -- Es funcion auxiliar de reversoPalabras
 -- OJO QUE SI HUBIERA ESPACIOS AL PRINCIPIO O AL FINAL LOS TOMA COMO PALABRAS
--- VACIAS, PERO ESTO ES BUENO PARA QUE DESPUES listaPalabrasAString AGREGUE
+-- VACIAS, PERO ESTO ES BUENO PARA QUE DESPUES listaPalabrasATexto AGREGUE
 -- DICHOS ESPACIOS. ES MEDIO DESPROLIJO PERO ANDA, PENSAR MEJOR FORMA
-listaPalabras :: String -> [Integer] -> Integer -> [String]
+listaPalabras :: Texto -> [Integer] -> Integer -> [Texto]
 -- Casos base
 listaPalabras s [] 0 = [s] -- Esto seria una sola palabra sin espacios
-listaPalabras s [] e = derecha s e : [] -- Esto seria la última palabra de un string
+listaPalabras s [] e = derecha s e : [] -- Esto seria la última palabra de un texto
 -- Casos que inician con espacio
 listaPalabras s (0:es) 0 = izquierda s 0 : listaPalabras s es 0
 listaPalabras (' ':s) (e:es) n = derecha (izquierda (' ':s) e) n : listaPalabras (' ':s) es e
@@ -101,21 +101,21 @@ listaPalabras s (e:es) n = derecha (izquierda s e) n : listaPalabras s es e
 
 -- Dada una lista de palabras, revierte cada una de ellas.
 -- Es funcion auxiliar de reversoPalabras
-reversoListaPalabras :: [String] -> [String]
+reversoListaPalabras :: [Texto] -> [Texto]
 reversoListaPalabras [] = []
 reversoListaPalabras (p:ps) = reverso p : reversoListaPalabras ps
 
--- Dada una lista de palabras genera un string con las palabras separadas por
+-- Dada una lista de palabras genera un texto con las palabras separadas por
 -- espacios.
 -- Es funcion auxiliar de reversoPalabras
-listaPalabrasAString :: [String] -> String
-listaPalabrasAString (p:[]) = p
-listaPalabrasAString (p:ps) = p ++ " " ++ listaPalabrasAString ps
+listaPalabrasATexto :: [Texto] -> Texto
+listaPalabrasATexto (p:[]) = p
+listaPalabrasATexto (p:ps) = p ++ " " ++ listaPalabrasATexto ps
 
 -- Dado un texto, devuelve cada palabra del texto al revés.
 -- Es funcion auxiliar de cifrarPalabrasReverso
-reversoPalabras :: String -> String
-reversoPalabras s = listaPalabrasAString (reversoListaPalabras (listaPalabras s (posicionesEspacios s) 0))
+reversoPalabras :: Texto -> Texto
+reversoPalabras s = listaPalabrasATexto (reversoListaPalabras (listaPalabras s (posicionesEspacios s) 0))
 
 cifrarPalabrasReverso :: Mensaje -> Mensaje
 cifrarPalabrasReverso (TextoClaro t) = CifradoPalabrasReverso (TextoClaro (reversoPalabras t))
